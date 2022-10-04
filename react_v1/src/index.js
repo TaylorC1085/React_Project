317b6bd05617452ada8e6b3e5c1c2d9d83979e13
Taylor <tcour10@hotmail.com>
2022-10-03 19:43:53 -0500

    Next commit.

--- react_v1/src/index.js
+++ react_v1/src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const jsxElement = <h1>This is a JSX element</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const headerStyles = {
+  backgroundColor: '#61DBFB',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5,
+}
+
+const header = (
+  <header style={headerStyles}>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 days of React</h1>
+      <h2>Getting started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Taylor Courchaine</p>
+      <small>August 8, 2022</small>
+    </div>
+  </header>
+)
+
+const mainStyles = {
+  backgroundColor: '#F3F0F5',
+}
+
+const main = (
+  <main style={mainStyles}>
+    <p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
+
+// JSX element, footer
+const footerStyles = {
+  backgroundColor: '#61DBFB',
+}
+const footer = (
+  <footer style={footerStyles}>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+// JSX element, app, a container or a parent
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(app, rootElement)
